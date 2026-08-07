39076fe52c8864a2534de2f1e0c1625745bdc717
Dyad <colgru05@gmail.com>
2026-08-07 21:37:47 +0300

    wrote 1 file(s)

--- src/pages/admin/AdminBuildingsPage.tsx
+++ src/pages/admin/AdminBuildingsPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import toast from 'react-hot-toast';
 import { Skeleton } from '../../components/Skeleton';
 import { AdminLayout } from '../../components/AdminLayout';
 import {
