0fe359363099c11b565f9bf70c280efe830dbc18
Dyad <vitaliekologriev@yandex.ru>
2026-08-18 17:12:19 +0300

    Update 1 file

--- vk_bot/config.py
+++ vk_bot/config.py
@@ -10,7 +10,7 @@ DB_PASSWORD = os.getenv('DB_PASSWORD', 'sAMogyg6sAMogyg')
 # Настройки Callback API
 CALLBACK_API_SECRET = os.getenv('CALLBACK_API_SECRET', 'IXLVL4sySPnsPO4E')
 CALLBACK_API_PORT = int(os.getenv('CALLBACK_API_PORT', 5000))
-CALLBACK_API_HOST = os.getenv('CALLBACK_API_HOST', '127.0.0.1')  # Только localhost, Nginx будет проксировать
+CALLBACK_API_HOST = os.getenv('CALLBACK_API_HOST', '0.0.0.0')  # Только localhost, Nginx будет проксировать
 CALLBACK_API_URL = os.getenv('CALLBACK_API_URL', 'https://alabuga.exowpn.ru/vk/callback')
 CALLBACK_CONFIRMATION_CODE = os.getenv('CALLBACK_CONFIRMATION_CODE', 'f7282f1c')
 GROUP_ID = os.getenv('GROUP_ID', '240686222')
